8f9d6f8d8b672ff9c8efdeb5600d301bd2b1ba6c
Weber <j.weber@unibw.de>
2016-09-21 14:32:21 +0200

    now

--- src/conflict/Konflikt01.java
+++ src/conflict/Konflikt01.java
@@ -1,5 +1,28 @@
 package conflict;
 
 public class Konflikt01 {
+	
+	public class Person {
+		
+		private String firstName;
+		private String lastName;
+		
+		public Person(String fN, String lN) {
+			this.firstName = fN;
+			this.lastName = lN;
+		}
 
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+		
+	}
+	
+	
 }
